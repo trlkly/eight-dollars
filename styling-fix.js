5aa31d7fb52670f356a0782e175dc20363facf1a
trlkly <trlkly@hotmail.com>
2022-11-18 14:17:09 -0600

    don't do this workaround on night-mode styles.

--- styling-fix.js
+++ styling-fix.js
@@ -1,3 +1,9 @@
-let style = document.createElement('style');
-style.textContent = `[data-eight-dollars-status="verified"] svg { color: black !important; }`
-document.head.appendChild(style);
+if (!document.cookie.includes('night_mode=1')) {
+  console.log(document.querySelector('meta[name="theme-color"]').content)
+  let style = document.createElement('style');
+  style.textContent = `[data-eight-dollars-status="verified"] svg { color: black !important; }`
+  document.head.appendChild(style);
+}
+
+// Requires refresh after changing from day to night mode or vice versa. 
+//Still have no idea why this workaround is necessary for usercripts, but not on the extension proper. 
