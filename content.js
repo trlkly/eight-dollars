eb737012ed3e48ab5937f40d70cb9ab81646c0fe
trlkly <trlkly@hotmail.com>
2023-05-07 17:41:18 -0500

    Update to 2.0, hopefully

--- content.js
+++ content.js
@@ -9,6 +9,13 @@ const defaultConfig = {
   },
 };
 
+async function loadVerifiedUsers() {
+  const url = chrome.runtime.getURL("data/verified.txt");
+  const response = await fetch(url);
+  const text = await response.text();
+  return text.split("\n");
+}
+
 function createSettingsDomNode(items) {
   const settingsDomNode = document.createElement("div");
   settingsDomNode.id = "eight-dollars-settings";
@@ -19,11 +26,19 @@ function createSettingsDomNode(items) {
 
 function injectScript() {
   const s = document.createElement("script", { id: "eight-dollars" });
-  s.src = chrome.runtime.getURL("script.user.js");
+  s.src = chrome.runtime.getURL("script.js");
   s.onload = function () {
     this.remove();
   };
   document.head.appendChild(s);
+}
+
+function injectVerifiedUsers(verifiedUsers) {
+  const verifiedUsersDomNode = document.createElement("div");
+  verifiedUsersDomNode.id = "eight-dollars-verified-users";
+  verifiedUsersDomNode.style.display = "none";
+  verifiedUsersDomNode.innerText = JSON.stringify(verifiedUsers);
+  document.body.appendChild(verifiedUsersDomNode);
 }
 
 if (typeof GM_info !== "undefined" || typeof GM !== "undefined") {
