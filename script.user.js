a5bf790d3125f5b6972b85f07e9056fe9abed73a
trlkly <trlkly@hotmail.com>
2023-05-07 18:36:11 -0500

    Full update to 2.0

--- script.user.js
+++ script.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name        eight-dollars
 // @match       *://*.twitter.com/*
-// @grant       none
-// @version     1.6
+// @grant       GM_getResourceText
+// @version     2.0
 // @require     https://raw.githubusercontent.com/trlkly/eight-dollars/main/styling-fix.js
-// @require     https://raw.githubusercontent.com/trlkly/eight-dollars/main/content.js
+// @require     https://raw.githubusercontent.com/wseagar/eight-dollars/main/content.js
+// @require     https://raw.githubusercontent.com/trlkly/eight-dollars/main/content-mods.js
 // @require     https://raw.githubusercontent.com/wseagar/eight-dollars/main/script.js
-// @require     https://raw.githubusercontent.com/wseagar/eight-dollars/main/search.js
+// @resource    verified.txt https://raw.githubusercontent.com/wseagar/eight-dollars/main/data/verified.txt
 // @author      William Seagar & Walter Lim
 // @description eight-dollars can help you tell the difference between actual verified accounts and twitter blue users. Just install the extension and see the difference.
 // ==/UserScript==
